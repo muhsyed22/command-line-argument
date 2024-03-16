bcaf8f0987e79754b054b72d6a6f25d4becdc659
muhsyed22 <149222230+muhsyed22@users.noreply.github.com>
2024-03-16 15:26:55 +0530

    command line argument

---
+++ Greet.java
@@ -0,0 +1,9 @@
+class Greet{
+   public static void main(String[] p){
+         
+         System.out.println("Welcome"+p[0]);
+         float a=Float.parseFloat(p[1]);
+         float b=Float.parseFloat(p[2]);
+         System.out.println(a+b);
+      }
+}
